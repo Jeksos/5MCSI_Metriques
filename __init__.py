f3c5de3270a9c3aadae102c980513f1536b4f8e1
Jeksos <165167845+Jeksos@users.noreply.github.com>
2024-06-08 04:35:50 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,40 +1,10 @@
 from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
 from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-
                                                                                                                                        
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
-@app.route('/')
-def hello_world():
-    return render_template('hello.html')
-  
-@app.route('/tawarano/')
-def meteo():
-    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
-    raw_content = response.read()
-    json_content = json.loads(raw_content.decode('utf-8'))
-    results = []
-    for list_element in json_content.get('list', []):
-        dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c
-        results.append({'Jour': dt_value, 'temp': temp_day_value})
-    return jsonify(results=results)
-
-@app.route("/rapport/")
-def mongraphique():
-    return render_template("graphique.html")
-
-@app.route("/histogramme/")
-def monhistogramme():
-    return render_template("Histogramme.html")
-
-@app.route("/contact/")
-def MaPremiereAPI():
-    return render_template("contact.html")
+app = Flask(__name__)                                                                                                                     
 
 @app.route('/commits/data')
 def get_commits_data():
@@ -50,12 +20,42 @@ def get_commits_data():
         commit_minutes.append(date_object.minute)
     
     return jsonify(commit_minutes=commit_minutes)
- 
 
 @app.route("/commits/")
 def commits():
     return render_template("commits.html")
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
   
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
+
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
+
+@app.route("/rapport/")
+def graphique():
+    return render_template("graphique.html")
+
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
+  
+  
+  
+  
